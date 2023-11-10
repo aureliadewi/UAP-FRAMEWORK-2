991689b7b245c3fdb35daf39fd2a93e7ffc8b5a2
aureliadewi <aureldewi40@gmail.com>
2023-11-10 15:53:18 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('/mahasiswa')->group(function () {
+    Route::get('/pendaftaran', function () {
+        return 'Halaman pendaftaran untuk pendaftaran';
+    });
+
+    Route::get('/ujian', function () {
+        return 'Halaman Ujian untuk /ujian';
+    });
+
+    Route::get('/nilai', function () {
+        return 'Halaman nilai untuk /nilai';
+
+});
+});
+
+
+
+
+
+
